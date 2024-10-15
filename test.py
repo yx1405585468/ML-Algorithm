b3a69c568fd33e37088501ca18733656eaa32a29
yu.xin <1405585468@qq.com>
2024-10-15 15:58:40 +0800

    1

--- test.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    from sklearn.datasets import load_iris
-
-    x = load_iris().data
-    y = load_iris().target
-
-    for index, sample in enumerate(x):
-        print(sample)
-        exit()
